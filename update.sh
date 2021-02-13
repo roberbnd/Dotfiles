f83b2a4963327055918d56e6c85ea430c275158d
Roberto Banda <banda1915@gmail.com>
2021-02-12 23:51:27 -0600

    removed vim-plug

--- update.sh
+++ update.sh
@@ -8,5 +8,4 @@ yes | cp ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
 yes | cp ~/.config/nvim/init.vim nvim/init.vim
 yes | cp -r ~/.config/nvim/conf nvim
 yes | cp ~/.config/nvim/coc-settings.json nvim/coc-settings.json
-yes | cp ~/.config/nvim/autoload/plug.vim nvim/autoload/plug.vim
 yes | cp -r ~/.config/coc/ultisnips coc
